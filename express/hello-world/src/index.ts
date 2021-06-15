3d3c4b41afd3cc9d1730de48b71db3c676e57e82
jaymzee <jaymzee@users.noreply.github.com>
2021-06-15 16:01:31 -0400

    express hello-world indenting

--- express/hello-world/src/index.ts
+++ express/hello-world/src/index.ts
@@ -3,19 +3,20 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    // res.send('Hello World!');
+    res.status(404).send("can't find that");
 });
 
 app.get('/books/', (req, res) => {
-  res.send('books');
+    res.send('books');
 });
 
 app.get('/books/:bookid', (req, res) => {
-  const bookid = req.params.bookid;
-  res.send(`book: ${bookid}`);
+    const bookid = parseInt(req.params.bookid, 10);
+    res.send(`book: ${bookid}`);
 });
 
 app.listen(port, () => {
-// tslint:disable-next-line:no-console
-  console.log(`Example app listening at http://localhost:${port}`);
+    // tslint:disable-next-line:no-console
+    console.log(`Example app listening at http://localhost:${port}`);
 });
