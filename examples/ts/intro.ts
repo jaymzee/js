22d713cebdceca9f6f621b82726f74986d911a59
jaymzee <jaymzee@users.noreply.github.com>
2021-08-21 00:29:14 -0400

    intro.ts 2 space indent

--- examples/ts/intro.ts
+++ examples/ts/intro.ts
@@ -1,31 +1,31 @@
 interface Person {
-    name: string;
-    age: number;
+  name: string;
+  age: number;
 }
 
 const p1 = {
-    name: 'joe',
-    age: 42,
-    when: new Date()
+  name: 'joe',
+  age: 42,
+  when: new Date()
 }
 
 // typescript is structural. p1 is a Person
 const p1_: Person = p1;
 
-// implements will insure the methods are provided but it isn't strictly
+// implements will ensure the methods are provided but it isn't strictly
 // necessary for Student to be a Person (typescript is structural)
 class Student implements Person {
-    name: string;
-    age: number;
-    private secret: number = 9;
-    constructor(name: string, age: number) {
-        this.name = name;
-        this.age = age;
-    }
-    speak(): void {
-        console.log(`hi my name is ${this.name} and my secret ` +
-                    `is ${this.secret}`);
-    }
+  name: string;
+  age: number;
+  private secret: number = 9;
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+  speak(): void {
+    console.log(`hi my name is ${this.name} and my secret ` +
+                `is ${this.secret}`);
+  }
 }
 
 // easy to make an immutable type
@@ -44,48 +44,48 @@ const s1: Shape = { kind: "circle", radius: 5 }
 const s2: Shape = { kind: "square", x: 8 }
 
 function foo(s: Shape) {
-    switch (s.kind) {
-        case "circle":
-            console.log(`its a circle with radius ${s.radius}`);
-            break;
-        case "square":
-            console.log(`its a square with side ${s.x}`);
-            break;
-        default:
-            break;
-    }
+  switch (s.kind) {
+    case "circle":
+      console.log(`its a circle with radius ${s.radius}`);
+      break;
+    case "square":
+      console.log(`its a square with side ${s.x}`);
+      break;
+    default:
+      break;
+  }
 }
 
 foo(s1);
 foo(s2);
 
 function firstish<T extends { length: number }>(t1: T, t2: T): T {
-    return t1.length > t2.length ? t1 : t2;
+  return t1.length > t2.length ? t1 : t2;
 }
 
 const z = firstish([2,3], [1]);
 console.log(z);
 
 interface Speaks {
-    speak()
+  speak()
 }
 
 const s3 = {
-    speak(): void {
-        console.log('hi');
-    }
+  speak(): void {
+    console.log('hi');
+  }
 }
 
 class Potato {
-    speak() {
-        console.log('im a potato');
-    }
+  speak() {
+    console.log('im a potato');
+  }
 }
 
 const s4 = new Potato();
 
 function bar(s: Speaks) {
-    s.speak();
+  s.speak();
 }
 
 bar(s3);
