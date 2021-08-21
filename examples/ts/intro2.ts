28fa76863fe85dd5d5363673051fe4e7853f2e84
jaymzee <jaymzee@users.noreply.github.com>
2021-08-21 01:01:31 -0400

    ts intro2

--- examples/ts/intro2.ts
+++ examples/ts/intro2.ts
@@ -1,3 +1,34 @@
 import * as fs from 'fs';
 const buf = fs.readFileSync('intro2.ts');
 console.log(buf.toString());
+
+interface SquareConfig {
+  color?: string;
+  width?: number;
+}
+
+function createSquare(config: SquareConfig) {
+  const sqr = { color: "white", area: 100 };
+
+  if (config.color) {
+    sqr.color = config.color;
+  }
+
+  if (config.width) {
+    sqr.area = config.width * config.width;
+  }
+
+  return sqr;
+}
+
+const redSquare = createSquare({color: 'red'});
+console.log(redSquare);
+
+interface Point {
+  readonly x: number;
+  readonly y: number;
+}
+
+const p1: Point = { x: 2, y: 3 };
+// p1.x = 5;
+
